5ec20d1fd54ffd8f649adbd82072833c8aa51425
Alena <Alena1118@icloud.com>
2025-07-20 17:25:58 +0700

    Реализовала выпадающий список со странами

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import 'reset-css';
 import './style.scss';
+import intlTelInput from 'intl-tel-input';
+
+const telInputs = document.querySelectorAll('.tel-input');
+
+telInputs.forEach(item => {
+  intlTelInput(item, {
+    initialCountry: 'ru',
+    loadUtils: () => import('intl-tel-input/utils'),
+  });
+});
